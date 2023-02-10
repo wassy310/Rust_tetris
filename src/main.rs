b35bcb89fa099090a134247383b1dd0425115953
wassy310 <ryg.wsza@gmail.com>
2023-02-10 14:29:08 +0900

    Add: collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::{thread, time};
 
+const FIELD_WIDTH:  usize = 11 + 2;
+const FIELD_HEIGHT: usize = 20 + 1;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+#[derive(Clone, Copy)]
 enum BlockKind {
     I, O, S, Z, J, L, T,
 }
@@ -62,6 +67,17 @@ struct Position {
     y: usize,
 }
 
+fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if field[y+pos.y+1][x+pos.x] & BLOCKS[block as usize][y][x] == 1 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn main() {
     let field = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -94,8 +110,11 @@ fn main() {
 
     // println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    for _ in 0..5 {
+    for _ in 0..30 {
         let field_buf = field;
+        if !is_collision(&field, &pos, BlockKind::I) {
+            pos.y += 1;
+        }
         for y in 0..4 {
             for x in 0..4 {
                 if BLOCKS[BlockKind::I as usize][y][x] == 1 {
@@ -104,10 +123,9 @@ fn main() {
             }
         }
 
-        pos.y += 1;
         // println!("\x1b[H");
-        for y in 0..21 {
-            for x in 0..13 {
+        for y in 0..FIELD_HEIGHT {
+            for x in 0..FIELD_WIDTH {
                 if field_buf[y][x] == 1 {
                     print!("[]");
                 } else {
